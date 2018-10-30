a2f0627fb534cfc082bc0d98514913fcda030d87
Plinio Vinicius <43052488+graett@users.noreply.github.com>
2018-10-30 14:36:01 -0300

    Atividade02
    Algoritmo básico de java que armazena números em váriáveis do tipo int e imprime a soma entre eles.

---
+++ Atividade02.java
@@ -0,0 +1,14 @@
+package Atividade02;
+
+public class Atividade02 {
+
+	public static void main(String[] args) {
+		int num1 = 20;
+		int num2 = 10;
+		int soma= (num1+num2);
+		
+		System.out.println("A soma dos números "+ num1+" e "+num2+" é : "+soma);
+
+	}
+
+}
