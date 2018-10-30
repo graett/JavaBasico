1813fdd6dfa2434051605e7a91407ba55f6247d0
Plinio Vinicius <43052488+graett@users.noreply.github.com>
2018-10-30 14:55:15 -0300

    Atividade03
    Algoritmo básico de java que utlizia variáveis do tipo float e imprime o resultado da divisão entre dois números.

---
+++ Atividade03.java
@@ -0,0 +1,14 @@
+package Atividade03;
+
+public class Atividade03 {
+
+	public static void main(String[] args) {
+		float num1 = 10;
+		float num2 = 20;
+		float divisao = (num1/num2);
+		
+		System.out.println("A divisão entre os números "+num1+" e "+num2+" é "+divisao);
+
+	}
+
+}
