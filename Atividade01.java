7b8ae5d40fac2d3b2c83c5d6ef5aa07088de236a
Plinio Vinicius <43052488+graett@users.noreply.github.com>
2018-10-30 14:22:01 -0300

    Atividade001
    Algoritmo básico de java que apresenta armazenamento em variáveis do tipo String e o uso do SOUT

---
+++ Atividade01.java
@@ -0,0 +1,14 @@
+package atividade01;
+
+public class Atividade01 {
+
+	public static void main(String[] args) {
+		String nome = "Paulo";
+		String endereco = "Rua das flores,84";
+		String telefone= "99345-0004";
+		
+		System.out.println("O "+nome+" domiciliado no endereço "+endereco+" e telefone "+ telefone+ " não possui pendências.");
+
+	}
+
+}
